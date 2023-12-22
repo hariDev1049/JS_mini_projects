ee58eeffb309f673bfff0a48940b80338e92927a
hariDev1049 <146807261+hariDev1049@users.noreply.github.com>
2023-12-22 14:16:00 +0530

    Todo App with Javascript
    Simple Todo app with all CRUD operation with JavaScript.
    Concepts covered.
    1. DOM manipulation
    2. CSS Flexbox 
    3. CRUD Operation in JS

---
+++ Todo_App/test.js
@@ -0,0 +1,93 @@
+const addTodoButton = document.querySelector("#add")
+let input = document.querySelector("#input")
+let newTodo = document.querySelector("#container")
+
+let delNodes = document.querySelectorAll(".delete")
+let editNodes = document.querySelectorAll(".edit")
+
+deleteTodo();
+editTodo();
+
+addTodoButton.addEventListener("click",(e)=>{
+    if(input.value ===''){
+        alert("Enter a valid Todo..!")
+        return
+    }
+    createNewTodo(input.value); 
+    input.value = ''
+
+    delNodes = document.querySelectorAll(".delete")
+    editNodes = document.querySelectorAll(".edit")
+    deleteTodo()
+    editTodo()
+},false)
+
+
+function createNewTodo(input){
+    let textNode = createInputText(input)
+    let editButton = createButtons(true);
+    let delButton = createButtons(false);
+    let rootDiv = createRootDiv();
+    rootDiv.appendChild(textNode);
+    rootDiv.appendChild(editButton);
+    rootDiv.appendChild(delButton);
+    // newTodo.insertBefore(rootDiv,newTodo.firstChild);
+    newTodo.appendChild(rootDiv);
+}
+
+
+function createInputText(input){
+    const todo = document.createElement('div');
+    todo.className='todo'
+    const addText = document.createTextNode(input);
+    todo.append(addText);
+    return todo;
+}
+
+function createButtons(value){
+    const button = document.createElement('input');
+    button.type='button'
+    if(value){ 
+    button.className='button edit'
+    button.value='Edit Todo'
+    button.id = 'edit'
+    }
+    else{
+        button.className='button delete'
+        button.value='Delete Todo'
+        button.id='delete'
+    }
+    return button;
+}
+
+function createRootDiv(){
+    const div = document.createElement('div');
+    div.className='new_todo'
+    div.id='new_todo'
+    return div;
+}
+
+function deleteTodo(){
+    delNodes.forEach((node)=>{
+        node.addEventListener("click", (e)=>{
+            delTodo(node)
+        },false)
+    })
+}
+
+function editTodo(){
+    editNodes.forEach((node) => {
+        node.addEventListener("click", (e)=>{
+            let editTodo = node.parentNode.firstElementChild
+            input.value = editTodo.innerHTML
+            delTodo(node)
+        },false)
+    })
+}
+
+function delTodo(node){
+    let deletedTodo = node.parentNode
+            if(deletedTodo.className === 'new_todo'){
+                deletedTodo.parentNode.removeChild(deletedTodo);
+            }
+}
